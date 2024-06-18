492beb1f21573ef51085dca15d910ae6465988d2
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-18 19:04:54 -0300

    feat: add Header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import { StyledHeader } from "./styledHeader"
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <img src="#" alt="Logo" />
+            <h1>Poke<span className="red">Finder</span></h1>
+            <p>Tema</p>
+        </StyledHeader>
+    )
+}
---
+++ src/components/header/styledHeader.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0rem 4rem 1rem;
+    // position: sticky;
+    // top: 0;
+
+    span {
+        margin-left: 4px;
+        color: red;
+    }
+`
