0d40ad76f17b76145243846314e97bc7666bdc1e
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-18 19:04:23 -0300

    feat: add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './assets/styles/App.css'
+import { Footer } from './components/footer/footer'
+import { Header } from './components/header/header'
+import { Hero } from './components/hero/hero'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Hero />
+      <Footer />
     </>
   )
 }
