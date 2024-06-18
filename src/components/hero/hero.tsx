047863ab49e48fd78e0498c0bcdb0a366817bb93
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-18 19:05:25 -0300

    feat: add Hero component

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,15 @@
+import { PokeCard } from "../poke-card/pokeCard"
+import { StyledHero } from "./styledHero"
+
+export const Hero = () => {
+
+    
+    const pName = ""
+    const pImage = ""
+
+    return (
+        <StyledHero>
+            <PokeCard pName={"pName"} pImage={"pImage"} />
+        </StyledHero>
+    )
+}
---
+++ src/components/hero/styledHero.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledHero = styled.main`
+    width: 100%;
+    flex: 1;
+    padding: 50px 200px;
+`
