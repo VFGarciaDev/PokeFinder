ba58dbb53f2a1a1db6c6a98001f606a3c1dd4aa1
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-21 10:58:31 -0300

    style: add more style features

--- src/components/hero/styledHero.tsx
+++ src/components/hero/styledHero.tsx
@@ -3,5 +3,28 @@ import styled from "styled-components";
 export const StyledHero = styled.main`
     width: 100%;
     flex: 1;
-    padding: 50px 200px;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+
+    .card-container {
+        width: 100%;
+        display: grid;
+        grid-template: min-content / repeat(auto-fit, minmax(150px, min-content));
+        justify-content: space-evenly;
+        gap: 40px 20px;
+    }
+
+    .btnSeeMore {
+        padding: 10px 20px;
+        font-size: 1.4rem;
+        border-radius: 9999px;
+        border: 0;
+        background-color: #FF3B3F;
+        &:hover {
+            cursor: pointer;
+        }
+    }
 `
--- src/components/poke-card/styledPokeCard.tsx
+++ src/components/poke-card/styledPokeCard.tsx
@@ -1,22 +1,31 @@
 import styled from "styled-components";
 
-export const StyledPokeCard = styled.article`
+export const StyledPokeCard = styled.a`
     position: relative;
-    width: 200px;
+    width: 150px;
     aspect-ratio: 0.75;
-    background-color: red;
-
+    display: flex;
+    overflow: hidden;
+    border-radius: 10px;
+    background-color: #1A1A1A;
+    cursor: pointer;
+    transition: transform 0.3s ease-out;
+    &:hover {
+        transform: scale(1.2);
+    }
+    
     p {
         position: absolute;
         top: 0;
         width: 100%;
         text-align: center;
         padding: 2px 0;
-        background-color: lightblue;
+        color: #FF3B3F;
+        background-color: #f2f2f2;
     }
-
     img {
         display: block;
-
+        padding-top: 28px;
+        width: 100%;
     }
 `
