c6662b91e9c9778271b1d627f913e18e29062303
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-21 10:58:03 -0300

    feat: add api content

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,15 +1,54 @@
-import { PokeCard } from "../poke-card/pokeCard"
 import { StyledHero } from "./styledHero"
+import { PokeCard } from "../poke-card/pokeCard"
+import { fetchPokemonData, getPokemonList } from "../../json/data"
+import { useEffect, useState } from "react"
+
+interface Pokemon {
+    name: string;
+    url: string;
+}
+interface PokemonData {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: Pokemon[];
+}
 
 export const Hero = () => {
+    const [pokeCount, setPokeCount] = useState(10)
+    const [data, setData] = useState<PokemonData | null>(null)
+    const [pokeInfo, setPokeInfo] = useState<any[]>([])
 
-    
-    const pName = ""
-    const pImage = ""
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetchPokemonData(pokeCount)
+            setData(response)
+            const p = await getPokemonList(pokeCount)
+            setPokeInfo(p)
+            console.log(p)
+        }
+        fetchData()
+        console.log(pokeInfo)
+    }, [pokeCount])
+
+    const handleGetMorePokemons = () => {
+        console.log(pokeCount)
+        setPokeCount(prevCount => prevCount + 10)
+        console.log(pokeCount)
+        console.log("oi")
+    }
 
     return (
         <StyledHero>
-            <PokeCard pName={"pName"} pImage={"pImage"} />
+            <div>
+                <p>Count: <span>{data && data.count}</span></p>
+            </div>
+            <section className="card-container">
+                {pokeInfo.map((value, index) => (
+                    <PokeCard key={index} pName={value.name} pImage={value.sprites.front_default} />
+                ))}
+            </section>
+            <button className="btnSeeMore" onClick={handleGetMorePokemons}>Ver mais</button>
         </StyledHero>
     )
 }
