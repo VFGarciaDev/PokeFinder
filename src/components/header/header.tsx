a5cfd353ecac30082adcef6a83aa8fee4384c306
VGarcia <vfeitosa.garcia@gmail.com>
2024-07-04 17:06:44 -0300

    refactor: add StyleComponent to root

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,27 @@
-import { StyledHeader } from "./styledHeader"
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0rem 4rem 1rem;
+    background-color: #2c2c2c;
+    color: #ffffff;
+    // position: sticky;
+    // top: 0;
+
+    span {
+        margin-left: 4px;
+        color: red;
+    }
+`
 
 export const Header = () => {
     return (
         <StyledHeader>
             <img src="#" alt="Logo" />
-            <h1>Poke<span className="red">Finder</span></h1>
+            <h1>Poke<span>Finder</span></h1>
             <p>Tema</p>
         </StyledHeader>
     )
